bb5e80a8bca8a17daf73ad07b074a69cb6a61149
Calvine Otieno <nyarangaotieno@gmail.com>
2022-05-27 16:42:26 +0300

    feat: add database connection
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable",
+		"ambassador",
+		"pg",
+		"pleasechangeme",
+		"ambassador",
+		"5432",
+	)
+
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
