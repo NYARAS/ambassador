8a0273c2a5268cfd7e3288f1de0f7ab70baad46e
Calvine Otieno <nyarangaotieno@gmail.com>
2022-05-31 13:40:33 +0300

    chore: add login endpoint and jwt
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/NYARAS/go-ambassador
 go 1.17
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofiber/fiber/v2 v2.34.0
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
 	gorm.io/driver/postgres v1.3.6
 	gorm.io/gorm v1.23.5
 )
@@ -24,7 +26,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.37.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -1,9 +1,97 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"strconv"
+	"time"
+
+	"github.com/NYARAS/go-ambassador/src/database"
+	"github.com/NYARAS/go-ambassador/src/models"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(ctx *fiber.Ctx) error {
+	var data map[string]string
+
+	err := ctx.BodyParser(&data)
+	if err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		ctx.Status(400)
+		return ctx.JSON(fiber.Map{
+			"message": "passwords do not match",
+		})
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
+
+	user := models.User{
+		FirstName:    data["first_name"],
+		LastName:     data["last_name"],
+		Email:        data["email"],
+		Password:     password,
+		IsAmbassador: false,
+	}
+
+	database.DB.Create(&user)
+
+	return ctx.JSON(user)
+}
+
+func Login(ctx *fiber.Ctx) error {
+	var data map[string]string
+
+	err := ctx.BodyParser(&data)
+	if err != nil {
+		return err
+	}
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		ctx.Status(fiber.StatusBadRequest)
+		return ctx.JSON(fiber.Map{
+			"message": "Invalid credentials!",
+		})
+	}
+
+	err = bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"]))
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest)
+		return ctx.JSON(fiber.Map{
+			"message": "Invalid credentials!",
+		})
+	}
+
+	payload := jwt.StandardClaims{
+		Subject:   strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte("secret"))
+
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest)
+		return ctx.JSON(fiber.Map{
+			"message": "Invalid credentials!",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	ctx.Cookie(&cookie)
+
 	return ctx.JSON(fiber.Map{
-		"message": "hello",
+		"message": "Success",
 	})
+
 }
