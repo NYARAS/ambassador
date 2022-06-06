7d91fa4b55789172a7bd68ec4aa8531e1f7b2072
Calvine Otieno <nyarangaotieno@gmail.com>
2022-06-06 16:44:33 +0300

    chore: register order model
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- src/database/db.go
+++ src/database/db.go
@@ -28,5 +28,5 @@ func Connect() {
 }
 
 func AutoMigrate() {
-	DB.AutoMigrate(models.User{}, models.Product{}, models.Link{})
+	DB.AutoMigrate(models.User{}, models.Product{}, models.Link{}, models.Order{})
 }
