242bcf085c8f95f8f15118232b0a7df0242fbc2f
Calvine Otieno <nyarangaotieno@gmail.com>
2022-08-17 16:56:03 +0300

    feat: add create links, rankings and stats for ambassadors
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- src/controllers/userController.go
+++ src/controllers/userController.go
@@ -13,3 +13,24 @@ func Ambassador(ctx *fiber.Ctx) error {
 
 	return ctx.JSON(users)
 }
+
+func Rankings(ctx *fiber.Ctx) error {
+	var users []models.User
+
+	database.DB.Find(&users, models.User{
+		IsAmbassador: true,
+	})
+
+	var result []interface{}
+
+	for _, user := range users {
+		ambassador := models.Ambassador(user)
+
+		ambassador.CalculateRevenue(database.DB)
+		result = append(result, fiber.Map{
+			user.Name(): ambassador.Revenue,
+		})
+	}
+
+	return ctx.JSON(result)
+}
